386c7b3188f7daaf35533ca5c22c54d738ee318c
Jeremy Lankford <jwlankford@gmail.com>
2026-07-29 21:28:30 -0400

    fix(cors): update CORS middleware to allow all localhost dev origins

--- app/main.py
+++ app/main.py
@@ -32,14 +32,13 @@ Designed with dynamic schema extensibility to allow businesses to map their inte
 )
 
 # Configure CORS for Vue frontend communication
-if settings.CORS_ORIGINS:
-    app.add_middleware(
-        CORSMiddleware,
-        allow_origins=settings.CORS_ORIGINS,
-        allow_credentials=True,
-        allow_methods=["*"],
-        allow_headers=["*"],
-    )
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(api_router, prefix=settings.API_V1_STR)
 
